990c56d6be69b6501da64cb6893b2db23b3c7db1
Gabriel Dias <117274518+DaysGabriel@users.noreply.github.com>
2024-04-29 19:27:41 +0000

    implemented all the versions of the "insert" method

--- BST/recursiveBST/recursiveBstNoTemplate/BST.cpp
+++ BST/recursiveBST/recursiveBstNoTemplate/BST.cpp
@@ -5,3 +5,38 @@ BST::BST(int _info)
     root = new Node(_info);
 }
 
+void BST::insert(int _info, Node* &root)
+{
+    if (node == nullptr)
+        root = new Node(_info);
+    else if (_info > root->info)
+        insert(_info, root->right);
+    else
+        insert(_info, root->left);
+}
+
+void BST::insert(int _info, Node** root)
+{
+    if (*root == nullptr)
+        *root = new Node(_info);
+    else if (_info > *(root)->info)
+        insert(_info, &(*root)->right);
+    else
+        insert(_info, &(*root)->left);
+}
+
+void BST::insertPrivate(int _info)
+{
+    return _insertPrivate(_info, root);
+}
+
+Node* BST::_insertPrivate(int _info, Node* root)
+{
+    if (root == nullptr)
+        root = new Node(_info);
+    else if (_info > root->info)
+        root->right = _insertPrivate(_info, root->right);
+    else
+        root->left = _insertPrivate(_info, root->left);
+    return root;
+}
