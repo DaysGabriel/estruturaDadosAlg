dc76351becb7342c60d07a7638c08bd8e537b7df
Gabriel Dias <117274518+DaysGabriel@users.noreply.github.com>
2024-04-29 17:18:03 +0000

    The class "node" was created

---
+++ BST/recursiveBST/recursiveBstNoTemplate/node.cpp
@@ -0,0 +1,3 @@
+#include "node.hpp"
+
+Node::Node(int _info):info(_info), left(nullptr), right(nullptr){}
---
+++ BST/recursiveBST/recursiveBstNoTemplate/node.hpp
@@ -0,0 +1,13 @@
+#ifndef BST_HPP
+#define BST_HPP
+
+class Node
+{
+public:
+    int info;
+    Node* left;
+    Node* right;
+    Node (int _info);
+};
+
+#endif
