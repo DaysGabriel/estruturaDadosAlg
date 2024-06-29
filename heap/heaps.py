6d30d7ac81e7657bfa099a4addaf17dd39c810a6
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-06-29 16:23:25 -0300

    Update heaps.py

--- heap/heaps.py
+++ heap/heaps.py
@@ -1,15 +1,13 @@
 """
-usage: heap(MAXIMA), heap(MINIMA). A única diferença da máxima pra mínima é que uma compara
-usando (>) e a outra usando (<) 
-"""
-"""
-Methods I know are right:
 1. __init__
-2. insert
-3. delete
-4. update
-5. __swap
-6. __climb
+2. __swap
+3. __climb
+4. __descend
+5. __buildHeap
+6. __buildHeap2
+7. insert
+8. delete b
+9. update
 """
 
 class MaxHeap:
@@ -21,45 +19,13 @@ class MaxHeap:
             self.array.append("#")
         self.array.extend(_array)
         self.__buildHeap(self.array)
-    #private methods
-    def __swap(self, _list, index1, index2):
-        temp = _list[index1]
-        _list[index1] = _list[index2]
-        _list[index2] = temp
-    def __climb(self, indexElem):
-        pai = indexElem//2
-        if pai >= 1 :
-            if self.array[indexElem] > self.array[pai]:
-                self.__swap(self.array,indexElem,pai)
-                self.__climb(pai)
-
-    def __descend(self, indexElem):
-        j = indexElem*2
-        if (j < len(self.array)-1) :
-            if self.array[j+1] > self.array[j] :
-                j += 1
-        if (j <= len(self.array) -1) and (self.array[j] > self.array[indexElem]) :
-            self.__swap(self.array,indexElem,j)
-            self.__descend(j)
-
-
-    #O(n)
-    def __buildHeap(self, array):
-        # for (int i = n/2; i > 0; i--)
-        for i in range((len(self.array)-1)//2, 0, -1):
-            self.__descend(i)
-    #O(n/2)
-    def __buildHeap2(self, array):        
-        for i in range(2,len(array)-1):
-            self.__climb(i)
-
     #public methods
     def insert(self, elem) :
         self.array.append(elem)
-        self.__climb(len(self.array)-1)
+        self.__climb(len(self.array))
     
     def delete(self) :
-        self.__swap(self.array, 1, len(self.array)-1)
+        self.__swap(self.array, 1, len(self.array))
         self.array.pop()
         self.__descend(1)
 
@@ -71,15 +37,44 @@ class MaxHeap:
             self.array[i] = newValue
             self.__descend(self,i)
 
-    def heapSort(_list):
+    def heapSort(self,_list):
         if (len(self.array) > 1) :
-            self.__swap(self.array[1], self.array[len(self.array)-1])
+            self.__swap(self.array, 1, len(self.array))
             _list.append(self.array.pop())
             self.__descend(1)
-            heapSort()
+            self.heapSort(_list)
 
     def delete(self):
         self.array.clear()
+
+    #private methods
+    def __swap(self, _list, index1, index2):
+        temp = _list[index1]
+        _list[index1] = _list[index2]
+        _list[index2] = temp
+    def __climb(self, indexElem):
+        pai = indexElem//2
+        if pai >= 1 :
+            if self.array[indexElem] > self.array[pai]:
+                self.__swap(self.array,indexElem,pai)
+                self.__climb(pai)
+    def __descend(self, indexElem):
+        j = indexElem*2
+        if (j < len(self.array)) :
+            if self.array[j+1] > self.array[j] :
+                j += 1
+        if (j <= len(self.array)) and (self.array[j] > self.array[indexElem]) :
+            self.__swap(self.array,indexElem,j)
+            self.__descend(j)
+    #O(n)
+    def __buildHeap(self, array):
+        # for (int i = n/2; i > 0; i--)
+        for i in range(len(self.array)//2, 0, -1):
+            self.__descend(i)
+    #O(nlog_{n})
+    def __buildHeap2(self, array):        
+        for i in range(2,len(array)):
+            self.__climb(i)
 
 def display(_list):
     print("[", end="")
